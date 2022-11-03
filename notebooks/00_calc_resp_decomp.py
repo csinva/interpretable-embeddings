1943b1a94c58ff3076504e981bf1fce235e1554f
Chandan Singh <chandan_singh@berkeley.edu>
2022-11-02 22:17:01 -0700

    viz pcs

--- notebooks/00_calc_resp_decomp.py
+++ notebooks/00_calc_resp_decomp.py
@@ -3,7 +3,7 @@ import os
 import pickle as pkl
 import numpy as np
 import sys
-from sklearn.decomposition import PCA, NMF
+from sklearn.decomposition import PCA, NMF, FastICA
 path_to_file = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(join(path_to_file, '..'))
 import feature_spaces
@@ -36,22 +36,31 @@ def calc_PCs(out_dir):
     pkl.dump({'pca': pca}, open(
         join(out_dir, 'resps_pca.pkl'), 'wb'))
 
+    print('fitting ICA...')
+    ica = FastICA().fit(zRresp)
+    pkl.dump({'ica': ica}, open(
+        join(out_dir, 'resps_ica.pkl'), 'wb')) 
+
     print('fitting NMF...')
-    nmf = NMF().fit(zRresp - zRresp.min())
-    pkl.dump({'nmf': nmf}, open(
-        join(out_dir, 'resps_nmf.pkl'), 'wb'))
+    try:
+        nmf = NMF().fit(zRresp - zRresp.min())
+        pkl.dump({'nmf': nmf}, open(
+            join(out_dir, 'resps_nmf.pkl'), 'wb'))
+    except:
+        print('failed nmf!')
 
 def viz_PCs(out_dir):
-    pca = pkl.load(open(join(out_dir, 'resps_pca.pkl'), 'rb'))
-    pc_dir = 'pcs_train'
     os.makedirs(pc_dir, exist_ok=True)
-    for i in tqdm(range(10)):
-        viz_cortex.quickshow(pca['pca'].components_[i])
-        plt.savefig(join(pc_dir, f'pca_component_{i}.pdf'))
-        plt.savefig(join(pc_dir, f'pca_component_{i}.png'))
-        plt.close()
+    for k in ['pca', 'ica', 'nmf']:
+        decomp = pkl.load(open(join(out_dir, 'resps_pca.pkl'), 'rb'))
+        pc_dir = 'pcs_train'
+        for i in tqdm(range(10)):
+            viz_cortex.quickshow(decomp[k].components_[i])
+            plt.savefig(join(pc_dir, f'{k}_component_{i}.pdf'))
+            plt.savefig(join(pc_dir, f'{k}_component_{i}.png'))
+            plt.close()
 
 if __name__ == '__main__':
-    # calc_PCs(out_dir)
+    calc_PCs(out_dir)
     viz_PCs(out_dir)
 
