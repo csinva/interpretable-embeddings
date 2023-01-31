b8df15fc56b8e4e582f6d2ab65d3f1b8497d7462
Chandan Singh <chandan_singh@berkeley.edu>
2023-01-30 21:31:50 -0800

    move func to imodelsx

--- 02_fit_decoding.py
+++ 02_fit_decoding.py
@@ -22,6 +22,7 @@ import sys
 import data
 from tqdm import tqdm
 from sklearn import preprocessing
+from imodelsx.util import get_embs_llm
 
 
 def get_word_vecs(X: List[str], model='eng1000') -> np.ndarray:
@@ -41,31 +42,6 @@ def get_word_vecs(X: List[str], model='eng1000') -> np.ndarray:
 def get_bow_vecs(X: List[str], X_test: List[str]):
     trans = CountVectorizer().fit(X).transform
     return trans(X).todense(), trans(X_test).todense()
-
-
-def get_embs_llm(X: List[str], checkpoint: str):  # X_test: List[str],
-    """Return embeddings from HF model given checkpoint name
-    (Fixed-size embedding by averaging over seq_len)
-    """
-    pipe = pipeline("feature-extraction",
-                    model=checkpoint,
-                    truncation=True,
-                    device=0)
-
-    def get_emb(x):
-        return {'emb': pipe(x['text'])}
-    text = datasets.Dataset.from_dict({'text': X})
-    out_list = text.map(get_emb)['emb']
-    # out_list is (batch_size, 1, (seq_len + 2), 768)
-
-    # convert to np array by averaging over len (can't just convert the since seq lens vary)
-    num_examples = len(out_list)
-    dim_size = len(out_list[0][0][0])
-    embs = np.zeros((num_examples, dim_size))
-    logging.info('extract embs HF...')
-    for i in tqdm(range(num_examples)):
-        embs[i] = np.mean(out_list[i], axis=1)  # avg over seq_len dim
-    return embs
 
 
 def get_embs_fmri(X: List[str], model, save_dir_fmri, perc_threshold=98) -> np.ndarray:
